3b2a424260187fcb5c7bf0530246db11dbb49f72
FitAdam <adam.george617@gmail.com>
2020-08-10 20:59:33 +0100

    models, first webpage :sparkles:

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+    """The blog post."""
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.title
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import BlogPost
+
+def index(request):
+    """The home page for blogs. Show all posts."""
+    posts = BlogPost.objects.order_by('date_added')
+    context = {'posts': posts}
+    return render(request, 'blogs/index.html', context)
