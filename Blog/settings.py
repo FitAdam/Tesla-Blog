9a47bb6cabd51053efd362a3d47f4a204814de09
FitAdam <adam.george617@gmail.com>
2020-08-19 20:54:25 +0100

    critical fix3.

--- Blog/settings.py
+++ Blog/settings.py
@@ -135,6 +135,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
